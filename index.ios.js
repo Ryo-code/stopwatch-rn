976791fefd1c0a399404e60ad3f74bd0892700ca
Ryo <kanadajinlee@yahoo.ca>
2017-01-22 13:41:47 -0500

    Made the layout with flexbox, and even successfully used an argument that's written in Chinese - haha, cool

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,69 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+//IMPORT CODE WE NEED
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
+//CREATE REACT COMPONENT
 export default class stopwatch extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <View style={[styles.header, this.border('yellow')]}>
+          <View style={this.border('red')}>
+            <Text>
+              00:00.00
+            </Text>
+          </View>
+        <View style={this.border('green')}>
+          {this.startStopButton()}
+          {this.startLapButton()}
+        </View>
+      </View>
+
+
+        <View style={[styles.footer, this.border('blue')]}>
+          <Text>
+            A list of laps
+          </Text>
+        </View>
       </View>
     );
   }
+
+  startStopButton() {
+    return <View>
+      <Text>
+        Start
+      </Text>
+    </View>
+  }
+
+  startLapButton() {
+    return <View>
+      <Text>
+        Lap
+      </Text>
+    </View>
+  }
+
+  border(色) {
+    return {borderColor: 色, borderWidth: 4}
+  }
+
 }
 
+//STYLE THE REACT COMPONENT
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'stretch'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  header: {
+    flex: 1,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  footer: {
+    flex: 1,
+  }
+})
 
+//SHOW THE REACT COMPONENT ON THE SCREEN
 AppRegistry.registerComponent('stopwatch', () => stopwatch);
