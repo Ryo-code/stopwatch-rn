ba4387492e194e112e3e99b3a259673b99238127
Ryo <kanadajinlee@yahoo.ca>
2017-01-27 14:34:52 -0500

    Made the start button work and formatted the time (S2 L30)

--- index.ios.js
+++ index.ios.js
@@ -1,23 +1,42 @@
 //IMPORT CODE WE NEED
+import formatTime from 'minutes-seconds-milliseconds';
 import React, {Component} from 'react';
-import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight //just a fancy word for "button"
+} from 'react-native';
 
 //CREATE REACT COMPONENT
 export default class stopwatch extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      timeElapsed: null
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
+
         <View style={[styles.header, this.border('yellow')]}>
-          <View style={this.border('red')}>
+          <View style={[styles.timerWrapper, this.border('red')]}>
             <Text>
-              00:00.00
+              {formatTime(this.state.timeElapsed)}
             </Text>
           </View>
-        <View style={this.border('green')}>
-          {this.startStopButton()}
-          {this.startLapButton()}
+
+          <View style={[styles.buttonWrapper, this.border('green')]}>
+            {this.startStopButton()}
+            {this.startLapButton()}
+          </View>
+
+          <TouchableHighlight onPress={this.clickityClick.bind(this)}>
+            <Text>My test button...</Text>
+          </TouchableHighlight>
         </View>
-      </View>
 
 
         <View style={[styles.footer, this.border('blue')]}>
@@ -25,30 +44,49 @@ export default class stopwatch extends Component {
             A list of laps
           </Text>
         </View>
+
       </View>
     );
   }
 
   startStopButton() {
-    return <View>
+    return ( <TouchableHighlight underlayColor="gray" onPress={this.handleStartPress.bind(this)}>
       <Text>
         Start
       </Text>
-    </View>
+    </TouchableHighlight>
+    )
+  }
+
+  handleStartPress(){
+    const startTime = new Date();
+
+
+    //this function will automatically update the state of the timer every 30 milliseconds, giving the illusion of a stopwatch
+    setInterval( () => {
+       this.setState({
+        timeElapsed: new Date() - startTime
+      });
+    }, 30);
+  }
+
+  clickityClick(){
+    console.log('clicked!')
   }
 
   startLapButton() {
-    return <View>
+    return (
+    <TouchableHighlight>
       <Text>
         Lap
       </Text>
-    </View>
+    </TouchableHighlight>
+    )
   }
 
   border(色) {
     return {borderColor: 色, borderWidth: 4}
   }
-
 }
 
 //STYLE THE REACT COMPONENT
@@ -58,10 +96,21 @@ const styles = StyleSheet.create({
     alignItems: 'stretch'
   },
   header: {
-    flex: 1,
+    flex: 1
   },
   footer: {
-    flex: 1,
+    flex: 1
+  },
+  timerWrapper: { //Red area
+    flex: 5, //takes up 5/8ths of the available space
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonWrapper: { //Green area
+    flex: 3, //takes up 3/8ths of the available space
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   }
 })
 
