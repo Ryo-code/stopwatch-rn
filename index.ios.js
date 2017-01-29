59038b8df0cf3373d16e5201944c95c043d215e4
Ryo <kanadajinlee@yahoo.ca>
2017-01-28 22:37:26 -0500

    Fixed styling for laps after figuring out how to make them in ES6

--- index.ios.js
+++ index.ios.js
@@ -14,69 +14,79 @@ export default class stopwatch extends Component {
   constructor(props){
     super(props);
     this.state = {
-      timeElapsed: null
+      timeElapsed: null,
+      running: false,
+      startTime: null,
+      laps: [],
     };
   }
   render() {
     return (
       <View style={styles.container}>
 
-        <View style={[styles.header, this.border('yellow')]}>
-          <View style={[styles.timerWrapper, this.border('red')]}>
-            <Text>
+        <View style={styles.header}>
+          <View style={styles.timerWrapper}>
+            <Text style={styles.timer}>
               {formatTime(this.state.timeElapsed)}
             </Text>
           </View>
 
-          <View style={[styles.buttonWrapper, this.border('green')]}>
+          <View style={styles.buttonWrapper}>
             {this.startStopButton()}
-            {this.startLapButton()}
+            {this.lapButton()}
           </View>
-
-          <TouchableHighlight onPress={this.clickityClick.bind(this)}>
-            <Text>My test button...</Text>
-          </TouchableHighlight>
         </View>
 
-
-        <View style={[styles.footer, this.border('blue')]}>
-          <Text>
-            A list of laps
-          </Text>
+        <View style={styles.footer}>
+          {this.laps()}
         </View>
 
       </View>
     );
   }
 
+  laps() {
+    return this.state.laps.map( (time, index) => {
+      return (
+        <View style={styles.lap}>
+          <Text style={styles.lapText}>
+            Lap #{index + 1}
+          </Text>
+          <Text style={styles.lapText}>
+            {formatTime(time)}
+          </Text>
+        </View>
+      )
+    })
+  }
+
   startStopButton() {
-    return ( <TouchableHighlight underlayColor="gray" onPress={this.handleStartPress.bind(this)}>
+    const style = this.state.running ? styles.stopButton : styles.startButton;
+
+    return (
+      <TouchableHighlight
+        underlayColor="gray"
+        onPress={this.handleStartPress.bind(this)}
+        style={[styles.button, style]}
+        >
       <Text>
-        Start
+        {this.state.running ? 'Stop' : 'Start'}
       </Text>
     </TouchableHighlight>
     )
-  }
-
-  handleStartPress(){
-    const startTime = new Date();
-
-
-    //this function will automatically update the state of the timer every 30 milliseconds, giving the illusion of a stopwatch
-    setInterval( () => {
-       this.setState({
-        timeElapsed: new Date() - startTime
-      });
-    }, 30);
   }
 
   clickityClick(){
     console.log('clicked!')
   }
 
-  startLapButton() {
+  lapButton() {
     return (
-    <TouchableHighlight>
+    <TouchableHighlight
+      style={styles.button}
+      underlayColor="gray"
+      onPress={this.handleLapPress.bind(this)}
+      >
       <Text>
         Lap
       </Text>
@@ -84,8 +94,35 @@ export default class stopwatch extends Component {
     )
   }
 
-  border(色) {
-    return {borderColor: 色, borderWidth: 4}
+  handleLapPress(){
+    const lap = this.state.timeElapsed;
+
+    this.setState({
+      laps: [...this.state.laps, lap],
+      //ES5: laps: this.state.laps.concat([lap])
+      startTime: new Date()
+    });
+  }
+
+  handleStartPress(){
+      if (this.state.running) {
+        clearInterval(this.interval);
+        this.setState({running: false});
+        return //we want to return so that it doesn't continue
+      }    //the code below (which would start the timer again)
+
+
+    this.setState({
+      startTime: new Date()
+    });
+
+    //this function will automatically update the state of the timer every 30 milliseconds, giving the illusion of a stopwatch
+    this.interval = setInterval( () => {
+       this.setState({
+        timeElapsed: new Date() - this.state.startTime,
+        running: true,
+      });
+    }, 30);
   }
 }
 
@@ -111,6 +148,30 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center'
+  },
+  timer: {
+    fontSize: 60,
+  },
+  button: {
+    borderWidth: 2,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  startButton: {
+    borderColor: '#00CC00'
+  },
+  stopButton: {
+    borderColor: '#CC0000'
+  },
+  lap: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  lapText: {
+    fontSize: 30
   }
 })
 
